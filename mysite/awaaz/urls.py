fcbf6e774aa3b25f6dffa48996495f14a7840001
Anushka <anushka30singh07@gmail.com>
2022-12-24 01:37:07 +0530

    backend

--- mysite/awaaz/urls.py
+++ mysite/awaaz/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index') # when user come to the root/hompage, anything that is in index function of views.py will be executed or rendered
+    path('complaint', views.complaint, name="complaint")
 ]
--- mysite/awaaz/views.py
+++ mysite/awaaz/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1> Hey </h1>')
+    return render(request,'index.html')
+
+
+def complaint(request):
+    return render(request, 'index2.html')
+    
