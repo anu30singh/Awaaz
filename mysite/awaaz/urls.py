26e841a6956ed367c97351e5ff66c01e585a1ba3
Lokesh-Singh <77160070+L0-k1@users.noreply.github.com>
2022-12-24 00:30:55 +0530

    Add files via upload

---
+++ mysite/awaaz/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index') # when user come to the root/hompage, anything that is in index function of views.py will be executed or rendered
+]
